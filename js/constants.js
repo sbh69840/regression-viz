024203ae2210552fb4ff45c795d6dfacedd40789
Lavish Suren <singhlavishk@gmail.com>
2022-07-26 15:19:45 +0530

    Added Buttons to add DataPoints or Reset the Plot

---
+++ js/constants.js
@@ -0,0 +1,5 @@
+export const xAxisMinVal = -100;
+export const xAxisMaxVal = 100;
+export const yAxisMinVal = -100;
+export const yAxisMaxVal = 100;
+export const regressionData = [];
--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,7 @@
 export const roundTo1 = (value) => round(value, 1);
+export const none = (arr, callback) => !arr.some(callback)
 
 function round(value, precision) {
-    var multiplier = Math.pow(10, precision || 0);
-    return Math.round(value * multiplier) / multiplier;
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
 }
